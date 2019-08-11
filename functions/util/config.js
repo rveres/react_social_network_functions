626820f51bfb5249e8231e43aac460097fab2f1c
Robert Veres <thedeveloper733@gmail.com>
2019-08-11 18:42:37 -0400

    Refactoring code

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCM05xoBTA3lz-lk-OFF44RL3fc83xU3zk",
+  authDomain: "socialape-42312.firebaseapp.com",
+  databaseURL: "https://socialape-42312.firebaseio.com",
+  projectId: "socialape-42312",
+  storageBucket: "socialape-42312.appspot.com",
+  messagingSenderId: "400647386946",
+  appId: "1:400647386946:web:a85e7709eef359d0"
+};
