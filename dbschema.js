b5bfc5d2e878d787ed83c2f46c0647fb284d35cf
Robert Veres <thedeveloper733@gmail.com>
2019-08-12 10:46:49 -0400

    Adding getAuthenticatedUser function

--- dbschema.js
+++ dbschema.js
@@ -1,23 +1,47 @@
 let db = {
-    users: [
-        {
-            userId: 'dh23ggj5h32g543j5gf43',
-            email: 'user@email.com',
-            handle: 'user',
-            createdAt: '2019-03-15T10:59:52.798Z',
-            imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
-            bio: 'Hell, my name is user, nice to meet you',
-            website: 'https://user.com',
-            location: 'London, UK'
-        }
-    ],
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream body',
-            createdAt: '2019-08-11T19:28:13.881Z',
-            likeCount: 5,
-            commentCount: 3
-        }
-    ]
-}
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+      bio: "Hell, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "London, UK"
+    }
+  ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2019-08-11T19:28:13.881Z",
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+};
+
+const userDetails = {
+  // Redux
+  credentials: {
+    userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:52.798Z",
+    imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "London, UK"
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "hh7O5oWfWucVzGbHH2pa"
+    },
+    {
+      userHandle: "user",
+      screamId: "3IOnFoQexRcofs5OBXO"
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,8 @@ const {
   signup,
   login,
   uploadImage,
-  addUserDetails
+  addUserDetails,
+  getAuthenticatedUser
 } = require("./handlers/users");
 
 // Scream routes
@@ -21,5 +22,6 @@ app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("us-east1").https.onRequest(app);
