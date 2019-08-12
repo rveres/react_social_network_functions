3ae3cfda0df60dafc3ef4af894c26ad09228f813
Robert Veres <thedeveloper733@gmail.com>
2019-08-12 10:33:02 -0400

    Adding update profile logic

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,23 @@
 let db = {
+    users: [
+        {
+            userId: 'dh23ggj5h32g543j5gf43',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2019-03-15T10:59:52.798Z',
+            imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+            bio: 'Hell, my name is user, nice to meet you',
+            website: 'https://user.com',
+            location: 'London, UK'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
             body: 'this is the scream body',
             createdAt: '2019-08-11T19:28:13.881Z',
             likeCount: 5,
-            commentCount: 2
+            commentCount: 3
         }
     ]
 }
--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,8 +5,13 @@ const config = require("../util/config");
 const firebase = require("firebase");
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData } = require("../util/validators");
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails
+} = require("../util/validators");
 
+// Sign user up
 exports.signup = (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -65,6 +70,7 @@ exports.signup = (req, res) => {
     });
 };
 
+// Log user in
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
@@ -96,6 +102,21 @@ exports.login = (req, res) => {
     });
 };
 
+// Add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: "Details added successfully " });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Upload a profile image for user
 exports.uploadImage = (req, res) => {
   const BusBoy = require("busboy");
   const path = require("path");
